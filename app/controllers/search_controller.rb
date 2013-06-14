01a707d2d3473aba14aa30f59c16c3c4f116ac76
Ex Qwztis <xqwztis@gmail.com>
2013-06-14 20:24:07 +0400

    Autocomplete: Initial functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,19 @@
+# -*- encoding: utf-8 -*-
 class SearchController < ApplicationController
   def index
+    if params[:term]
+      args = params[:term].split
+      4.times do |i|
+        args[i] ||= ''
+        args[i] += '%'
+      end
+      condition = 'last_name LIKE ? and first_name LIKE ? and patronymic LIKE ? and dob LIKE ?'
+      @patients = Patient.where(condition, *args).limit(5)
+    end
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => @patients }
+    end
   end
 end
