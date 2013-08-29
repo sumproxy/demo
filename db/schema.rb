efcc11e544c015fcaaa1ea5b69f43dc12e33650a
Ex Qwztis <xqwztis@gmail.com>
2013-08-29 15:59:56 +0400

    Blank

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130822085445) do
+ActiveRecord::Schema.define(:version => 20130826083240) do
 
   create_table "addresses", :force => true do |t|
     t.string   "city"
