53258a0868e5bf225ee7ed64394f4187a2b6f119
Ex Qwztis <xqwztis@gmail.com>
2013-09-01 12:01:20 +0400

    Create admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -174,3 +174,6 @@ if GynecologicExamination::FreeFluidAmount.all.empty?
   GynecologicExamination::FreeFluidAmount.create(value: "большом")
   GynecologicExamination::FreeFluidAmount.create(value: "-- не задано --")
 end
+if User.where('is_root = ?', true).count.zero?
+  User.create(login: 'admin', last_name: '1', first_name: '1', patronymic: '1', password: 'admin', is_root: true)
+end
