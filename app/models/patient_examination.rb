2ac9bdd5b40e0ba9fa043e66053840b206fc0fb6
Ex Qwztis <xqwztis@gmail.com>
2013-09-12 16:12:44 +0400

    Fix displayed links to previous examinations

--- app/models/patient_examination.rb
+++ app/models/patient_examination.rb
@@ -1,10 +1,14 @@
+# encoding: UTF-8
 class PatientExamination < ActiveRecord::Base
   belongs_to :patient
   belongs_to :gynecologic_examination
-  attr_accessible :patient_id, :gynecologic_examination_id
+  attr_accessible :patient_id
+  attr_accessible :gynecologic_examination_id
   
   def examination_type
-    [:gynecologic_examination].each do |exam|
+    examinations = PatientExamination.reflections.collect{|k,v| k if v.macro == :belongs_to}
+    examinations.delete(:patient)
+    examinations.each do |exam|
       return exam unless self.send((exam.to_s + "_id").to_sym).nil?
     end
   end
