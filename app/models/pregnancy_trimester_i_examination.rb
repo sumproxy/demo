b16b8944ee1529c98bd560e317933f80e37ddd31
Ex Qwztis <xqwztis@gmail.com>
2013-11-19 15:12:36 +0300

    Some validation checks

--- app/models/pregnancy_trimester_i_examination.rb
+++ app/models/pregnancy_trimester_i_examination.rb
@@ -10,4 +10,21 @@ class PregnancyTrimesterIExamination < ActiveRecord::Base
   attr_accessible :uterus_length
   attr_accessible :uterus_transverse_size
   attr_accessible :findings_and_recommendations
+
+  validate :uterine_attributes
+  
+  def uterine_attributes
+    if gestation_age_in_weeks.nil? || gestation_age_in_weeks == 0
+      errors.add(:gestation_age_in_weeks, "Значение не задано или недопустимо")
+    end
+    if uterus_anteroposterior.nil? || uterus_anteroposterior == 0
+      errors.add(:uterus_anteroposterior, "Значение не задано или недопустимо")
+    end
+    if uterus_length.nil? || uterus_length == 0
+      errors.add(:uterus_length, "Значение не задано или недопустимо")
+    end
+    if uterus_transverse_size.nil? || uterus_transverse_size == 0
+      errors.add(:uterus_transverse_size, "Значение не задано или недопустимо")
+    end
+  end
 end
