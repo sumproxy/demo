caed4840a7087eed295d735de2257ff37c3a7ab9
Ex Qwztis <xqwztis@gmail.com>
2013-11-20 16:29:17 +0300

    Change Inflection rule for for fetus

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Demo::Application.initialize!
+
+ActiveSupport::Inflector.inflections do |inflect|
+ inflect.irregular 'fetus', 'fetuses'
+end
