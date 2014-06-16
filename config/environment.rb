711add47fa998e6152618d4153f923cf8ce84a99
Sum Proxy <sum.proxy@gmail.com>
2014-06-16 12:48:36 +0400

    Moved "fetus" inflection init to inflections.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Demo::Application.initialize!
-
-ActiveSupport::Inflector.inflections do |inflect|
- inflect.irregular 'fetus', 'fetuses'
-end
